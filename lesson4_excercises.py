0bd4e07108e87b92d2ac8cb4516da9f87a828a2f
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-14 14:45:36 +0200

    count_even

--- lesson4_excercises.py
+++ lesson4_excercises.py
@@ -118,3 +118,10 @@ def calculate_total(numbers):
         sum += numbers
     return sum
 
+def count_even(numbers):
+    count = 0
+    for number in numbers:
+        if number % 2 == 0:
+            count += 1
+    return count
+
