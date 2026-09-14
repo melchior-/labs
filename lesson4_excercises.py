e3045d760b5d4744ca16300fc7f1f327f7d6ba94
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-14 12:58:43 +0200

    greet, show_course_name and print_separator

---
+++ lesson4_excercises.py
@@ -0,0 +1,17 @@
+# Part A
+
+def greet():
+    print("Hello")
+
+def show_course_name():
+    print("Java")
+
+def print_separator():
+    print("")
+
+greet()
+greet()
+print_separator()
+show_course_name()
+show_course_name()
+print_separator()
