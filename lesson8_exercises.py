4ea3b4c9065ed21136d40922faf5856889536067
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-23 15:36:42 +0200

    New team class

--- lesson8_exercises.py
+++ lesson8_exercises.py
@@ -6,3 +6,21 @@ class BadTeam():
 
     def add_member(self, new_member):
         self.members.append(new_member)
+
+team1 = BadTeam("Hawks")
+team2 = BadTeam("Lions")
+
+team1.add_member("Martin")
+
+print(team1.members)
+print(team2.members)
+
+# The member gets added to both teams. memers=[] is a default argument, and Python creates that list once when the function is defined, not once per instance.
+
+class Team():
+    def __init__(self, name, members = None):
+        self.name = name
+        self.members = [] if members is None else members
+
+    def add_member(self, new_member):
+        self.member.append(new_member)
