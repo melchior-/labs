d2eed6a1e488154c79554cf8fb650d3b0e5c3959
Martin Pettersson <martin.pettersson@outlook.com>
2026-09-24 10:38:02 +0200

    Document classes

--- lesson10_exercises.py
+++ lesson10_exercises.py
@@ -1,3 +1,5 @@
+# Part A
+
 class EmailNotification:
     def send(self):
         return "Sent from Email"
@@ -19,3 +21,37 @@ list_of_items = [email, sms, push]
 for item in list_of_items:
     print(item.send())
 
+# Python uses duck typing: if an object has the method you call, it works
+
+# Part B
+
+class Document:
+    def __init__(self, title):
+        self.title = title
+
+    def describe(self):
+        return "Normal document"
+
+class PDFDocument(Document):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def describe(self):
+        return "PDF"
+
+class TextDocument(Document):
+    def __init__(self, title):
+        super().__init__(title)
+
+    def describe(self):
+        return "Text Document"
+
+pdf1 = PDFDocument("Bill")
+pdf2 = PDFDocument("CV")
+text1 = TextDocument("Receipt")
+text2 = TextDocument("Newspaper")
+
+list_of_documents = [pdf1, pdf2, text1, text2]
+
+for document in list_of_documents:
+    print(f"{document.title} {document.describe()}")
